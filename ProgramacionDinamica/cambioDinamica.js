3031b9164f662f125a8be1afec38d3fda051cc0c
Lucas Vega <lucasvega02@gmail.com>
2024-01-30 20:53:09 -0300

    Problema del cambio con Programacion Dinamica

--- ProgramacionDinamica/cambioDinamica.js
+++ ProgramacionDinamica/cambioDinamica.js
@@ -1,5 +1,5 @@
 /* 
-Problema del Cambio con Programacion Dinamica: 
+Problema del Cambio con Programación Dinámica: 
 
 - La idea es construir una tabla (o matriz) en la que cada celda represente el mínimo número de monedas necesarias para obtener una cantidad específica de dinero. Inicialmente, esta tabla se llena con valores infinitos, excepto para el caso base donde la cantidad de dinero es cero, que requiere cero monedas.
 
@@ -27,15 +27,61 @@ Ejemplo de tabla -> Tenemos monedas de 6, 4 y 1 para pagar 8
 ---------------------------------------
 6 |   |   |   |   |   |   |   |   |   |
 ---------------------------------------
+
+Formulas para definir valor en cada celda (tabla[i][j]):
+  - Si j = 0 ? 0
+  - Si i = 1 && di > j ? infinito // di es el valor de la denominacion en la posicion i. Es decir si la denominacion que tengo en la primer fila es mayor a lo que tengo que pagar. Basicamente, no puedo pagar
+  - Si i = 1 && di <= j ? tabla[i][j-1] + 1
+  - Si i > 1 && i > j ? tabla[i-1][j]
+  - Si i > 1 && di <= j ? min(tabla[i][j-di], tabla[i-1][j])
 */
 
 function cambioDinamica(candidatos, objetivo) {
+  let matriz = [];
+  let filas = candidatos.length;
+  let columnas = objetivo + 1;
 
-  let tabla = [[]]
+  // Llenamos la matriz con ceros
+  for (let i = 0; i < filas; i++) {
+      matriz.push(new Array(columnas).fill(0));
+  }
 
-  // Evaluamos qué nos conviene
+  // Generamos los valores con las fórmulas
+  for (let i = 0; i < filas; i++) {
+    for (let j = 0; j < columnas; j++) {
+        if (j === 0) {
+            matriz[i][j] = 0;
+        } else if (i === 0) {
+            if (candidatos[i] > j) {
+                matriz[i][j] = Infinity;
+            } else {
+                matriz[i][j] = matriz[i][j - candidatos[i]] + 1;
+            }
+        } else {
+            if (candidatos[i] > j) {
+                matriz[i][j] = matriz[i - 1][j];
+            } else {
+                matriz[i][j] = Math.min(matriz[i][j - candidatos[i]] + 1, matriz[i - 1][j]);
+            }
+        }
+    }
+  }
 
-  // Si i = 0 ? 
-    // Si i <= j ? tabla[i][j] = 
- 
+  // Mostramos la matriz obtenida
+  console.log("-------------------------------------------------------------------")
+  mostrarMatriz(matriz);
+  console.log("-------------------------------------------------------------------");
+
 }
+
+
+function mostrarMatriz(m) {
+  for (let fila of m) {
+      console.log(fila.join('\t'));
+  }
+}
+
+let cand = [1, 4, 6]
+let obj = 8
+
+cambioDinamica(cand, obj)
